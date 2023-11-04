2443f163bb24781734d459e3563e35326138b373
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-04 14:07:10 +0100

    feat: Order models

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,46 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
+from apps.payments.models import PAYMENT_FORMS
+
 # Create your models here.
+
+
+class Order(models.Model):
+    customer = models.ForeignKey("users.Customer", on_delete=models.CASCADE)
+    products = models.ManyToManyField("products.Product", through="OrderProduct")
+    date = models.DateField(auto_now_add=True)
+    payment_form = models.CharField(max_length=255, choices=PAYMENT_FORMS)
+    delivery_point = models.ForeignKey(
+        "products.DeliveryPoint", on_delete=models.SET_NULL, null=True
+    )
+
+    # Estado de la reserva
+    STATUS = (
+        ("No empezado", "No empezado"),
+        ("En posesión", "En posesión"),
+        ("Finalizado", "Finalizado"),
+    )
+    order_status = models.CharField(max_length=255, choices=STATUS)
+
+    @property
+    def total(self):
+        total = 0
+        for product in self.products.all():
+            total += product.price
+        return total
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey("Order", on_delete=models.CASCADE)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ("order", "product")
+
+    # Check start is before end_date when creating a cart product
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date must be before end date")
--- apps/payments/models.py
+++ apps/payments/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+PAYMENT_FORMS = (
+    ("A contrareembolso", "A contrareembolso"),
+    ("Tarjeta de crédito", "Tarjeta de crédito"),
+)
+
 
 class PaymentMethod(models.Model):
     # TODO: Add secure payment storage
