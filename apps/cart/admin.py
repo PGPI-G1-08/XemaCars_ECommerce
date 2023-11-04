d74cd8e4c2d2a2e132fb6509a95faf8613de50bd
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-04 12:40:06 +0100

    feat: Cart added

--- apps/cart/admin.py
+++ apps/cart/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+# Include the cart model in the admin site, so that we can also add and view the products in the cart within the cart details and creation pages.
+from .models import Cart, CartProduct
+
+
+class CartProductInline(admin.TabularInline):
+    model = CartProduct
+
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ()
+    search_fields = ()
+
+    inlines = [
+        CartProductInline,
+    ]
+
+
+admin.site.register(Cart, CartAdmin)
--- apps/cart/models.py
+++ apps/cart/models.py
@@ -1,3 +1,26 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
+
+
+class Cart(models.Model):
+    customer = models.OneToOneField(
+        "users.Customer", on_delete=models.CASCADE, primary_key=True
+    )
+    products = models.ManyToManyField("products.Product", through="CartProduct")
+
+
+class CartProduct(models.Model):
+    cart = models.ForeignKey("Cart", on_delete=models.CASCADE)
+    product = models.ForeignKey("products.Product", on_delete=models.CASCADE)
+    start_date = models.DateField(blank=True, null=True)
+    end_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        unique_together = ("cart", "product")
+
+    # Check start is before end_date when creating a cart product
+    def clean(self):
+        if self.start_date >= self.end_date:
+            raise ValidationError("Start date must be before end date")
