2939afba302ba70883d62fdcbe62bb2e5349a380
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-04 12:39:41 +0100

    feat: Products models added

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Product
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "price", "image_url")
+    search_fields = ("name",)
+
+
+admin.site.register(Product, ProductAdmin)
