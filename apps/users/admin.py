cc9a0db046435bf656ffecdecfac6da2a6ab1bb6
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-11-04 12:45:03 +0100

    fix: User admin models

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,21 +1,13 @@
 from django.contrib import admin
 
-from .models import Customer, DeliveryInfo
+from .models import Customer
 
 # Register your models here.
-
-
-class DeliveryInfoInline(admin.TabularInline):
-    model = DeliveryInfo
 
 
 class CustomerAdmin(admin.ModelAdmin):
     list_display = ("user", "phone_number")
     search_fields = ("user",)
-
-    inlines = [
-        DeliveryInfoInline,
-    ]
 
 
 admin.site.register(Customer, CustomerAdmin)
