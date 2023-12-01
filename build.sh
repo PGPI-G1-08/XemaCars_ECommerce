87d7598004dd38753af1ac33aee8192e982a89f4
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-12-01 20:24:39 +0100

    fix: Add missing migration to render

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python -m pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 
-python manage.py makemigrations cart opinions orders payments products users
+python manage.py makemigrations cart opinions orders payments products users complaints
 python manage.py migrate
 
 python manage.py loaddata populate/*
