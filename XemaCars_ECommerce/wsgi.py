e0ec030a44f290971b667c1a5411334111cf68b8
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2023-12-02 12:37:40 +0100

    fix: Stripe in render

--- XemaCars_ECommerce/wsgi.py
+++ XemaCars_ECommerce/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import stripe
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "XemaCars_ECommerce.settings")
 
+stripe.api_key = settings.STRIPE_SECRET_KEY
 application = get_wsgi_application()
